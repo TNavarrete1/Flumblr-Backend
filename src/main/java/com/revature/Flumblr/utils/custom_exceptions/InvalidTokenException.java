bd0110e9c829753aa39eebabf220f4d6967621a7
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-06-30 17:48:59 -0400

    resolve conflicts

--- src/main/java/com/revature/Flumblr/utils/custom_exceptions/InvalidTokenException.java
+++ src/main/java/com/revature/Flumblr/utils/custom_exceptions/InvalidTokenException.java
@@ -1,1 +1,7 @@
+package com.revature.Flumblr.utils.custom_exceptions;
 
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
