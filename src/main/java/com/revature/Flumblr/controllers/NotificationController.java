7a48c562b3eb17e94ab00aa1c3dd25f9dd3d3137
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-07-05 16:53:19 -0400

    remove unused imports

--- src/main/java/com/revature/Flumblr/controllers/NotificationController.java
+++ src/main/java/com/revature/Flumblr/controllers/NotificationController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.Flumblr.dtos.requests.NewCommentRequest;
 import com.revature.Flumblr.dtos.requests.NotificationRequest;
-import com.revature.Flumblr.dtos.responses.PostResponse;
 import com.revature.Flumblr.entities.Notification;
 import com.revature.Flumblr.services.NotificationService;
 import com.revature.Flumblr.services.TokenService;
