9eb96422a746333fb23dc338e2e35f952ebfe46d
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-07-06 15:39:45 -0400

    Update PostService.java

--- src/main/java/com/revature/Flumblr/services/PostService.java
+++ src/main/java/com/revature/Flumblr/services/PostService.java
@@ -36,11 +36,11 @@ import com.revature.Flumblr.entities.PostVote;
 @AllArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
-    public final BookmarksRepository bookmarksRepository;
+    private final BookmarksRepository bookmarksRepository;
     private final UserService userService;
     private final UserRepository userRepository;
-    public final PostVoteRepository postVoteRepository;
-    public final CommentRepository commentRepository;
+    private final PostVoteRepository postVoteRepository;
+    private final CommentRepository commentRepository;
 
     public List<Post> getFeed(String userId, int page) {
         User user = userService.findById(userId);
@@ -73,6 +73,77 @@ public class PostService {
         if (userPost.isEmpty())
             throw new ResourceNotFoundException("Post(" + postId + ") Not Found");
         return userPost.get();
+    }
+
+    public String getPostOwner(String postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + postId + " was not found"));
+        return post.getUser().getId();
+    }
+
+    public void deletePost(String postId) {
+        try {
+            postRepository.deleteById(postId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Post with id " + postId + " was not found");
+        }
+    }
+
+    public void createPost(MultipartHttpServletRequest req, String fileUrl, String userId) {
+
+        Optional<User> userOpt = userRepository.findById(userId);
+
+        User user = userOpt.get();
+
+        String message = req.getParameter("message");
+
+        if (message == null && fileUrl == null) {
+            throw new ResourceConflictException("Message or media required!");
+        }
+
+        String mediaType = req.getParameter("mediaType");
+        if (mediaType == null) {
+            throw new FileNotUploadedException("Media Type can not be empty!");
+        }
+
+        Post post = new Post(message, mediaType, fileUrl, user);
+
+        postRepository.save(post);
+
+    }
+
+    public List<PostResponse> getTrending(Date fromDate) {
+        List<Post> responses = postRepository.findAll();
+        List<PostResponse> resPosts = new ArrayList<PostResponse>();
+
+        for (Post userPost : responses) {
+            // Integer numberOfVotes = postVoteRepository.findAllByPost(userPost).size();
+            Double score = CalculateScore(userPost);
+            int checkWhen = fromDate.compareTo(userPost.getCreateTime());
+
+            if (checkWhen == -1 || checkWhen == 0) {
+                resPosts.add(new PostResponse(userPost, score));
+            }
+        }
+
+        Collections.sort(resPosts, new Comparator<PostResponse>() {
+            @Override
+            public int compare(PostResponse post1, PostResponse post2) {
+                double score1 = post1.getScore();
+                double score2 = post2.getScore();
+
+                if (score1 < score2) {
+                    return 1; // Return a positive value to indicate post2 should come before post1
+                } else if (score1 > score2) {
+                    return -1; // Return a negative value to indicate post1 should come before post2
+                } else {
+                    return 0; // Return 0 to indicate the scores are equal
+                }
+            }
+        });
+
+        List<PostResponse> limitedList = resPosts.subList(0, Math.min(resPosts.size(), 10));
+        return limitedList;
     }
 
     private Double CalculateScore(Post post) {
