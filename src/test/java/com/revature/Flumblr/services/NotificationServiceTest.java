495e8bdea9f8996a35363354a24afb29e9a636c9
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-07-12 12:41:41 -0400

    tests done

--- src/test/java/com/revature/Flumblr/services/NotificationServiceTest.java
+++ src/test/java/com/revature/Flumblr/services/NotificationServiceTest.java
@@ -1,6 +1,12 @@
 package com.revature.Flumblr.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.revature.Flumblr.dtos.requests.NotificationRequest;
+import com.revature.Flumblr.dtos.responses.NotificationResponse;
 import com.revature.Flumblr.entities.Notification;
 import com.revature.Flumblr.entities.NotificationType;
 import com.revature.Flumblr.entities.User;
@@ -35,33 +43,85 @@ public class NotificationServiceTest {
         User user = new User();
         NotificationType type = new NotificationType();
 
-        Notification notification = new Notification(message, user, link, type);
+        notificationService.createNotification(message, link, user, type);
 
-        verify(notificationRepository).save(notification);
+        verify(notificationRepository).save(any(Notification.class));
     }
 
     @Test
     void testDeleteNotification() {
+        String id = "id";
+        Notification notification = new Notification();
+        notification.setId(id);
 
+        when(notificationRepository.findById(id)).thenReturn(java.util.Optional.of(notification));
+
+        notificationService.deleteNotification(id);
+
+        verify(notificationRepository).delete(notification);
     }
 
     @Test
     void testFindById() {
+        String id = "id";
+        Notification notification = new Notification();
+        notification.setId(id);
+
+        when(notificationRepository.findById(id)).thenReturn(java.util.Optional.of(notification));
+
+        Notification result = notificationService.findById(id);
+
+        assertEquals(notification, result);
 
     }
 
     @Test
     void testGetNotificationByUser() {
+        String userId = "user";
+        User user = new User();
+        user.setId(userId);
+
+        List<Notification> notifications = new ArrayList<>();
+        List<NotificationResponse> responses = new ArrayList<>();
+
+        when(notificationRepository.findByUserOrderByCreateTimeDesc(user)).thenReturn(notifications);
+
+        List<NotificationResponse> results = notificationService.getNotificationByUser(userId);
+
+        assertEquals(responses, results);
 
     }
 
     @Test
     void testGetUnreadNotificationByUser() {
+        String userId = "user";
+        User user = new User();
+        user.setId(userId);
 
+        List<Notification> notifications = new ArrayList<>();
+        List<NotificationResponse> responses = new ArrayList<>();
+
+        when(notificationRepository.findByUserAndViewedOrderByCreateTimeDesc(user, false))
+                .thenReturn(notifications);
+
+        List<NotificationResponse> results = notificationService.getNotificationByUser(userId);
+
+        assertEquals(responses, results);
     }
 
     @Test
     void testReadNotification() {
+        NotificationRequest req = new NotificationRequest();
+        req.setNotificationId("notificationId");
+        req.setUserId("userId");
+        String id = "id";
+        Notification notification = new Notification();
+        notification.setId(id);
 
+        when(notificationRepository.findById(req.getNotificationId())).thenReturn(java.util.Optional.of(notification));
+
+        notificationService.readNotification(req);
+
+        verify(notificationRepository).save(notification);
     }
 }
